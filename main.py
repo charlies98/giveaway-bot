8fc8745ee0cb0913d200b1bc9d7f0b17e0fba236
charlies98 <soyalguncer@gmail.com>
2025-05-15 19:30:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID"))
 
 intents = discord.Intents.default()
